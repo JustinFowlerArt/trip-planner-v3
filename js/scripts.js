40b73786b945c3d10d61a06bf469f44eb1d4beba
Justin Fowler <jusfowle@gmail.com>
2020-10-13 20:05:50 -0400

    Fix javascript loop, add logo image

--- js/scripts.js
+++ js/scripts.js
@@ -31,18 +31,27 @@ const trips = [
     }
 ];
 
+// Log trips content to console
 for ( let i = 0; i < trips.length; i++ ) {
     console.log(`${trips[i].location}`);
     let expenseName = '';
     let expensePrice = 0;
-    for ( let j = 0; j < trips[i].expenses[j].length; j++ )
+    for ( let j = 0; j < trips[i].expenses.length; j++ )
     {
         expenseName = trips[i].expenses[j].expenseName;
         expensePrice = trips[i].expenses[j].expensePrice;
-    } 
-    console.log(`${expenseName}`);
-    console.log(`${expensePrice}`);
+        console.log(`${expenseName}`);
+        console.log(`${expensePrice}`);
+    }
     console.log(`${trips[i].total}`);
 }
 
 // Add class for creating trips
+
+//Add function to calucate and display trip total
+
+// Add function to add new expenses to trip
+// function addExpense(expenseName, expensePrice) {
+//     trips[0].expenses[0].expenseName.push(expenseName);
+//     trips[0].expenses[0].expensePrice.push(expensePrice);
+// }
