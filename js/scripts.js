c0a3de393e11bc61efcd50040271052eca21e852
Justin Fowler <jusfowle@gmail.com>
2020-11-10 20:15:15 -0500

    Refactor JS and add id's to elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 // Selectors
 const tripList = document.querySelector('.trip-list');
 const newTripButton = document.querySelector('.new-trip');
-const addExpenseButton = document.querySelectorAll('.add-expense')[0];
-const expenseList = document.querySelectorAll('.expense-list')[0];
 
 // Holds trip objects
 let trips = [];
+let counter = 0;
 
 // Blueprint to create new trip object
 class Trip {
@@ -13,6 +12,7 @@ class Trip {
         this.name = name;
         this.expenses = [];
         this.total = 0;
+        this.id = counter;
     }
 
     // Add new expense object to expenses array
@@ -37,134 +37,53 @@ class Expense {
     }
 }
 
-// Base for writing trip HTML to page
-let tripInfo = '';
-function tripContent() {
-    for ( let i = 0; i < trips.length; i++) {
-        tripInfo = `
-            <h2>${trips[i].name}</h2>
-            <ul class="expense-list column flex">
-                <button class="add-expense" id="${trips.length -1}">Add Expense</button>
-                <li class="trip-total flex">
-                    <p>Trip Total</p>
-                    <p class="total">$${trips[i].total}</p>
-                </li>
-            </ul>
-        `;
-    }
-}
-
-// Create trip nodes
-function tripDOM() {
-    let section = document.createElement("section");
+// Generate trip HTML section element and contents
+function getTripSectionElement (trip) {
+    const section = document.createElement('section');
+    section.id = `trip-section-${trip.id}`;
     section.className = "trip column flex";
-    section.id = trips.length -1;
-    section.innerHTML = tripInfo;
-    tripList.appendChild(section);
+    const tripHTMLContent = `        
+        <h2>${trip.name}</h2>
+        <ul class="expense-list column flex" id="expense-list-${trip.id}">
+        </ul>
+        <button class="add-expense" id="expense-button-${trip.id}">Add Expense</button>
+        <div class="trip-total flex">
+            <p>Trip Total</p>
+            <p class="total">$${trip.total}</p>
+        </div>
+    `;
+    section.innerHTML = tripHTMLContent;
+    return section
 }
 
-// Create new trip object
+// Button to create new trip object and populate it with HTML content
 newTripButton.addEventListener('click', () => {
     let tripName = prompt("Please name your trip");
-    trips.push( new Trip(tripName) );
-    tripContent();
-    tripDOM();
+    const newTrip = new Trip(tripName)
+    const sectionElement = getTripSectionElement(newTrip);
+    tripList.appendChild(sectionElement);
+    const newTripExpenseButton = document.querySelector(`#expense-button-${newTrip.id}`);
+    newTripExpenseButton.addEventListener('click', () => {
+        const newTripExpenseList = document.querySelector(`#expense-list-${newTrip.id}`);
+        let expenseName = prompt("Please name your expense");
+        let expensePrice = prompt("Please enter the expense amount");
+        newTrip.addExpense(expenseName, expensePrice);
+        const newExpenseItemElement = getExpenseListItemElement(newTrip);
+        newTripExpenseList.appendChild(newExpenseItemElement);
+    });
+    counter++;
 });
 
-// Create new expense object
-addExpenseButton.addEventListener('click', () => {
-    let expenseName = prompt("Please name your expense");
-    let expensePrice = prompt("Please enter the expense amount");
-    trips[0].addExpense(expenseName, expensePrice);
-    expenseDOM();
-});
+// Fix trips array
 
-// Create trip nodes
-function expenseDOM() {
+// Fix list item generation 
+function getExpenseListItemElement(trip) {
     let li = document.createElement("li");
     li.className = "expense flex";
-    li.id = trips[0].expenses.length -1;
+    li.id = `expense-item-${trip.id}`;
     li.innerHTML = `                
-        <p>${trips[0].expenses[0].name}</p> 
-        <p>$${trips[0].expenses[0].price}</p>
+        <p>${trip.expenses[0].name}</p> 
+        <p>$${trip.expenses[0].price}</p>
     `;
-    expenseList.appendChild(li);
-    expenseList.insertBefore(li, addExpenseButton);
+    return li;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// denver.expenses.push( new Expense('Airfare', 125) );
-// denver.expenses.push( new Expense('Rental Car', 275) );
-
-// const trips = [
-//     {
-//         location: "Denver, CO",
-//         expenses:
-//         [
-//            {
-//                 expenseName: "Airfare",
-//                 expensePrice: 125
-//            },
-//            {
-//                 expenseName: "Rental Car",
-//                 expensePrice: 275
-//             }
-//         ],
-//         //Add function to calucate and display trip total
-//         total: 400
-//     },
-//     {
-//         location: "Seattle, WA",
-//         expenses:
-//         [
-//            {
-//                 expenseName: "Airfare",
-//                 expensePrice: 350
-//            },
-//            {
-//                 expenseName: "Airbnb",
-//                 expensePrice: 500
-//             }
-//         ],
-//         total: 850
-//     }
-// ];
-
-// // Log trips content to console
-// for ( let i = 0; i < trips.length; i++ ) {
-//     console.log(`${trips[i].location}`);
-//     let expenseName = '';
-//     let expensePrice = 0;
-//     for ( let j = 0; j < trips[i].expenses.length; j++ )
-//     {
-//         expenseName = trips[i].expenses[j].expenseName;
-//         expensePrice = trips[i].expenses[j].expensePrice;
-//         console.log(`${expenseName}`);
-//         console.log(`${expensePrice}`);
-//     }
-//     console.log(`${trips[i].total}`);
-// }
-
-// Add function to add new expenses to trip
-// function addExpense(expenseName, expensePrice) {
-//     trips[0].expenses[0].expenseName.push(expenseName);
-//     trips[0].expenses[0].expensePrice.push(expensePrice);
-// }
